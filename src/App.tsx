5ef229b895570bb481b2495e4b01f272eccbf249
Juan Cruz Alderete <juancruzalderete11@gmail.com>
2025-08-18 18:17:12 -0300

    Remove Zustand state management and related code from App component; simplify to render Login page only

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,4 @@
-import { Button } from "@/components/ui/button";
-import { create } from "zustand";
 import Login from "./pages/login";
-
-type Store = {
-  count: number;
-  inc: () => void;
-};
-
-const useStore = create<Store>()((set) => ({
-  count: 1,
-  inc: () => set((state) => ({ count: state.count + 1 })),
-}));
 
 function App() {
   return <Login />;
